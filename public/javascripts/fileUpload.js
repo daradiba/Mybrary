188d5702bfdf0887d60c79040b443c13cad635c9
daradiba <daradiba@yahoo.com>
2020-07-31 00:46:32 +0300

    Finish Project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,15 +1,32 @@
-// Register all the plugins we want to install
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+// Load the styles from the root element
+// Otherwise, when this file loads, the styles are not loaded yet 
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-    // Set the height / width ratio, as they are in books/index.ejs
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
 
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+
+    // Register all the plugins we want to install
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+
+    FilePond.setOptions({
+        // Set the height / width ratio, as they are in books/index.ejs
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+
+    FilePond.parse(document.body);
+}
+
